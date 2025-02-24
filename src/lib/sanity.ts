8e8d8ce9f204fca2b6dff103fe0adea188110952
Barry Hoggard <bhoggard@gmail.com>
2025-02-23 21:18:03 -0500

    draft with loading from sanity

---
+++ src/lib/sanity.ts
@@ -0,0 +1,32 @@
+import { createClient } from '@sanity/client'
+
+export const client = createClient({
+  projectId: 'ng5yto4p',
+  dataset: 'production',
+  apiVersion: '2023-05-03',
+  useCdn: false
+})
+
+export async function getVenuesGroupedByNeighborhood() {
+  const today = new Date().toISOString()
+  return await client.fetch(`
+    *[_type == "neighborhood"] {
+      _id,
+      name,
+      "venues": *[_type == "venue" && references(^._id)] {
+        _id,
+        name,
+        address,
+        url,
+        "events": *[_type == "event" && references(^._id) && startDate <= $today && endDate >= $today] | order(startDate asc) {
+          _id,
+          name,
+          startDate,
+          endDate,
+          url
+        }
+      }
+    }
+  `, { today })
+}
+
